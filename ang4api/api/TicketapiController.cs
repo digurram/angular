b5c2b967c4c0b55c6706057d8ecd8519ba38c9df
Narendranath Gurram <naren.7090@outlook.com>
2018-10-19 19:13:53 +0530

    working code

--- ang4api/api/TicketapiController.cs
+++ ang4api/api/TicketapiController.cs
@@ -11,6 +11,7 @@ namespace ang4api.api
     [Authorize(Roles = "Admin,BasicUser")]
     public class TicketapiController : BaseAPIController
     {
+        #region "Basic data"
         [HttpGet]
         public HttpResponseMessage Get()
         {
@@ -94,5 +95,34 @@ namespace ang4api.api
         {
             return BTypeMaster();
         }
+        #endregion
+
+        #region "file upload"
+        /*
+        [HttpPost]
+        public HttpResponseMessage Post()
+        {
+            HttpResponseMessage result = null;
+            var httpRequest = HttpContext.Current.Request;
+            if (httpRequest.Files.Count > 0)
+            {
+                var docfiles = new List<string>();
+                foreach (string file in httpRequest.Files)
+                {
+                    var postedFile = httpRequest.Files[file];
+                    var filePath = HttpContext.Current.Server.MapPath("~/" + postedFile.FileName);
+                    postedFile.SaveAs(filePath);
+                    docfiles.Add(filePath);
+                }
+                result = Request.CreateResponse(HttpStatusCode.Created, docfiles);
+            }
+            else
+            {
+                result = Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+            return ToJson(result);
+        }
+       */
+        #endregion
     }
 }
