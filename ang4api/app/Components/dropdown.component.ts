e7051459b3e832be51a1a57636e69b41b9858e58
Narendranath Gurram <naren.7090@outlook.com>
2018-10-31 11:12:48 +0530

    implementing ControlValueAccessor in dropdown component

--- ang4api/app/Components/dropdown.component.ts
+++ ang4api/app/Components/dropdown.component.ts
@@ -1,59 +1,36 @@
-﻿import { Component, Input, SimpleChange, OnInit } from '@angular/core';
+﻿import { Component, Input, SimpleChange, OnInit, forwardRef } from '@angular/core';
 import { IkeyValuePair } from '../Model/keyValuePair';
-import { Form, FormGroup, ControlValueAccessor } from '@angular/forms';
+import { Form, FormGroup, ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
     selector: 'dropdown',
-    template: ` <select  class="form-control"   >
+    template: ` <select  class="form-control" [disabled]="disabled"  (change)="onChange($event.target.value)" (blur)="onTouched()" >
                                 <option value=-1>Select</option>
-                                <option *ngFor="let coll of drpcollection" [value]="coll.Id" [selected]="coll.Id ==selecId"  (change)="pushChange($event)">{{coll.keyValue}}</option>
-                            </select>`
+                                <option *ngFor="let coll of drpcollection" [value]="coll.Id" [selected]="coll.Id ==value"  (change)="pushChange($event)">{{coll.keyValue}}</option>
+                            </select>`,
+    providers: [{ provide: NG_VALUE_ACCESSOR, useExisting: forwardRef(() => DropdownComponent), multi: true }]
 })
 
 export class DropdownComponent implements ControlValueAccessor {
 
-    private _selecId: number;
+    value: number;
+    onChange: () => void;
+    onTouched: () => void;
+    disabled: boolean;
     show: boolean = false;
+    constructor() { }
 
-    onChange = (_: any) => { };
-    onTouched = () => { };
 
-    @Input() ctrname: string;
-
-    constructor() {
-        console.log(this.selecId);
-        this._selecId = this.selecId;
-    }
-
-    @Input()
-    set selecId(val: number) {
-        console.log('in set ->' + val);
-        this._selecId = val;
-        //this.parentForm.controls[this.ctrname].value = val;
-    }
-    get selecId(): number {
-        console.log('in get ->' + this._selecId);
-        return this._selecId;
-    }
-
-    //@Input() parentForm:FormGroup
 
     @Input() drpcollection: IkeyValuePair[];
 
     writeValue(value: any): void {
-        this._selecId = value || -1;
+        this.value = value || -1;
     }
 
-    pushChange(event: any) {
-        this._selecId  = event.target.value;
-        console.log('new value is ' + this._selecId);
-        this.writeValue(this._selecId);
-        this.onChange(this._selecId);
-    }
-
-
-    registerOnChange(fn: (_: any) => {}): void { this.onChange = fn; }
-    registerOnTouched(fn: () => {}): void { this.onTouched = fn; }
+    registerOnChange(fn: any): void { this.onChange = fn; }
+    registerOnTouched(fn: any): void { this.onTouched = fn; }
     setDisabledState(isDisabled: boolean): void {
+        this.disabled = isDisabled;
     }
 }
